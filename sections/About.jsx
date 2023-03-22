5f72501a95fd8e948dd8d9fdc6ee8b5bfedf1d26
Tandid Alam <tandid.alam@gmail.com>
2023-03-22 17:12:51 -0400

    About Section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,37 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
-    About section
+  <section className="sm:p-16 xs:p-8 px-6 py-12 relative z-10">
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer()}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="2xl:max-w-[1280px] w-full mx-auto flex justify-center items-center flex-col"
+    >
+      <TypingText title="| About Yuta Media" textStyles="text-center" />
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-slate-300"
+      >
+        <span className="font-extrabold text-white">Yuta Media </span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
+        risus in erat venenatis ultrices quis at est.
+      </motion.p>
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow-down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
   </section>
 );
 
