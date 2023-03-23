0885db86ffce9003656e923f11f548d9ac514fa3
Tandid Alam <tandid.alam@gmail.com>
2023-03-23 15:24:17 -0400

    New Feature Section Finished

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,7 +1,24 @@
-const NewFeatures = () => (
-  <div>
-    New Features
-  </div>
-);
+const NewFeatures = (props) => {
+  const { imgUrl, title, subtitle } = props;
+  return (
+    <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+      <div className="flex justify-center items-center w-[70px] h-[70px] rounded-[24px] bg-slate-600">
+        <img
+          src={imgUrl}
+          alt="icon"
+          width="0"
+          height="0"
+          className="w-1/2 h-1/2 object-contain"
+        />
+      </div>
+      <h1 className="mt-[26px] font-bold text-[24px] leading-[30px] text-white">
+        {title}
+      </h1>
+      <p className="flex-1 mt-[16px] font-normal text-[18px] text-slate-300 leading-[32px]">
+        {subtitle}
+      </p>
+    </div>
+  );
+};
 
 export default NewFeatures;
--- sections/About.jsx
+++ sections/About.jsx
@@ -3,7 +3,6 @@
 import { motion } from 'framer-motion';
 import { TypingText } from '../components';
 
-import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
