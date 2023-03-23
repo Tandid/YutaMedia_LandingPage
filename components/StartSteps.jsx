e8e0f48871f4236024ededced565f0bf1a7ed36c
Tandid Alam <tandid.alam@gmail.com>
2023-03-23 15:10:01 -0400

    Get Started Section Done

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,7 +1,15 @@
-const StartSteps = () => (
-  <div>
-    start steps
-  </div>
-);
+const StartSteps = (props) => {
+  const { number, text } = props;
+  return (
+    <div className="flex justify-center items-center flex-row">
+      <div className="flex justify-center items-center w-[70px] h-[70px] rounded-[24px] bg-slate-600">
+        <p className="font-bold text-[20px] text-white">0{number}</p>
+      </div>
+      <p className="flex-1 ml-[30px] font-normal text-[18px] text-slate-300 leading-[32px]">
+        {text}
+      </p>
+    </div>
+  );
+};
 
 export default StartSteps;
